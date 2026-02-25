6918d8950b686dd52b39a7f4450dd3d43575acc2
ejuniorr88 <ejuniorrrr@gmail.com>
2026-02-25 01:09:45 -0300

    Update supabase_client.py

--- core/supabase_client.py
+++ core/supabase_client.py
@@ -1,7 +1,7 @@
-from supabase import create_client
 import streamlit as st
+from supabase import create_client
 
-supabase = create_client(
-    st.secrets["SUPABASE_URL"],
-    st.secrets["SUPABASE_KEY"]
-)
+def get_supabase():
+    url = st.secrets["SUPABASE_URL"]
+    key = st.secrets["SUPABASE_ANON_KEY"]
+    return create_client(url, key)
